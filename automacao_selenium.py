87a468df08f29ea6578e92770961976e0e674b94
Giovanni Rodrigues <giovannirodrigues.2012@gmail.com>
2024-08-06 20:00:29 -0300

    Preenchendo o input de username

--- automacao_selenium.py
+++ automacao_selenium.py
@@ -1,8 +1,18 @@
-# Importando webdriver da biblioteca selenium
+# Importando webdriver e By da biblioteca selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+# Importando biblioteca time para adicionar uma pausa no nosso codigo
+from time import sleep
 
 # Abrindo o navegador para começar a automacação, utilizando o webdriver
 chrome = webdriver.Chrome()
-
 # Abrindo uma url
 chrome.get('https://www.saucedemo.com/')
+
+#Adicionando uma pausa na execução do codigo
+sleep(1)
+
+#Buscando o elemento de input de username utilizando o xpath
+input_username = chrome.find_element(By.XPATH, '//input[@id="user-name"]')
+#Inserindo o valor no campo de username
+input_username.send_keys('standard_user')
