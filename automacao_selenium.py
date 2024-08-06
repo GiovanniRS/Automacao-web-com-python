13f7e7aecd549b9b7ea31ae85dfff2f2219ce1d2
GiovanniRS <giovannirodrigues.2012@gmail.com>
2024-08-06 19:00:08 -0300

    Criando estrutura inicial para automacao

---
+++ automacao_selenium.py
@@ -0,0 +1,5 @@
+# Importando webdriver da biblioteca selenium
+from selenium import webdriver
+
+# Abrindo o navegador para começar a automacação, utilizando o webdriver
+chrome = webdriver.Chrome()
